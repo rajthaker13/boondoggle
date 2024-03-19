5ecd60f63caa642f9081ce9029ae16934470390c
rajthaker13 <69283649+rajthaker13@users.noreply.github.com>
2024-03-19 11:21:26 -0700

    niceeee

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import Manager from "./screens/Manager/Manager";
 import Payment from "./screens/Payment/Payment";
 import Settings from "./screens/Settings/Settings";
 import BoondogggleAI from "./screens/BoondoggleAI/BoondoggleAI";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const supabase = createClient(
   "https://gwjtbxxhdsqrelswpgdi.supabase.co",
@@ -25,45 +26,9 @@ const supabase = createClient(
 );
 
 export default function App() {
-  // async function signInWithTwitter() {
-  //   const { data, error } = await supabase.functions.invoke("twitter-login-3");
-  //   console.log(data);
-  //   localStorage.setItem("oauth_token", data.url.oauth_token);
-  //   localStorage.setItem("oauth_secret", data.url.oauth_token_secret);
-  //   window.open(data.url.url, "_self");
-  // }
-  // async function captureOauthVerifier() {
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   const oauthVerifier = urlParams.get("oauth_verifier");
-
-  //   // Now oauthVerifier contains the value of oauth_verifier parameter
-  //   console.log("Verifier " + oauthVerifier);
-  //   localStorage.setItem("oauth_verifier", oauthVerifier);
-  //   const token = localStorage.getItem("oauth_token");
-  //   const secret = localStorage.getItem("oauth_secret");
-  //   console.log(token);
-  //   console.log(secret);
-  //   const { data, error } = await supabase.functions.invoke("get-twitter-dms", {
-  //     body: { token: token, secret: secret, oauthVerifier: oauthVerifier },
-  //   });
-  //   console.log(data);
-  //   const { data2, error2 } = await supabase.auth.signInWithOAuth({
-  //     provider: "twitter",
-  //   });
-  //   console.log(data2);
-  // }
-
-  // useEffect(() => {
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   if (urlParams.has("oauth_verifier")) {
-  //     captureOauthVerifier();
-  //   }
-  // }, []);
-
-  // <UnifiedDirectory workspace_id={"65c02dbec9810ed1f215c33b"} />
-
   return (
     <Router>
+      <SpeedInsights />
       <Routes>
         <Route path="/" element={<Landing db={supabase} />} />
         <Route path="/privacy" element={<Privacy db={supabase} />} />
